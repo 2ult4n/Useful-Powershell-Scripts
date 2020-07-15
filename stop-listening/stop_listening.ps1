b3a7d60a557d535fe4e3d23bdccf03860d7d2fcb
2ult4n <isultanfx@gmail.com>
2020-07-16 01:29:05 +0300

    Create stop_listening.ps1

---
+++ stop-listening/stop_listening.ps1
@@ -0,0 +1,21 @@
+﻿$Process = Get-Process -Id (Get-NetTCPConnection -LocalPort $args[0]).OwningProcess
+if ($Process){
+    Write-Host "Process Name: $($Process.Name) Process Id: $($Process.Id) "
+    
+    $title    = "Conform"
+    $question = "Are you sure you want to stop the following process? Process Name: $($Process.Name) Process Id: $($Process.Id) "
+    $choices  = '&Yes', '&No'
+
+    $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+
+    if ($decision -eq 0) {
+        Stop-Process -Id $Process.Id
+        Write-Host "Process with the name $($Process.Name) have been stopped"
+    } else {
+        Write-Host 'Cancelled'
+    }
+}else{
+    Write-Host "There is no process listening to the port $($args[0])" 
+}
+
+
